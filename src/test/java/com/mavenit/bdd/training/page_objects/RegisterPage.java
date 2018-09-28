ef7830c17f505ff12f2e27a339148e9dab132288
rkesireddy <rakeshreddy255@gmail.com>
2018-09-28 17:34:01 +0100

    Update RegisterPage.java

--- src/test/java/com/mavenit/bdd/training/page_objects/RegisterPage.java
+++ src/test/java/com/mavenit/bdd/training/page_objects/RegisterPage.java
@@ -22,7 +22,7 @@ public class RegisterPage extends DriverFactory {
     @FindBy(id = "Password")
     private WebElement password_txt;
 
-    @FindBy(id = "ConfirmPassword")
+    @FindBy(id = "ConfirmPasswordss")
     private WebElement confirmPasswordTxt;
 
 
